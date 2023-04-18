27b855a49c21b00cda965c815afa74b2f8a418ed
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-04-18 02:19:21 -0400

    added js files

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const logger = require("morgan");
+const transactionsController = require("./controllers/transactionsController.js");
+
+app.use(expresss.json()); //parse incoming data
+app.use(cors());
+
+app.use(logger("dev")); //used with morgan package
+
+app.use("/transactions", transactionsController);
+
+app.use((req, res, next) => {
+  console.log("This code runs for every request");
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Budgeting App");
+})
+
+//404 page
+app.get("*", (req, res) => {
+  res.status(404).json({ error: "Page not found" });
+})
+
+module.exports = app;
---
+++ models/transaction.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    id: 1,
+    item_name: "Salary",
+    amount: 5000,
+    date: "2022-01-31",
+    from: "Employer",
+    category: "Income"
+  },
+  {
+    id: 2,
+    item_name: "Rent",
+    amount: 1200,
+    date: "2022-01-01",
+    from: "Landlord",
+    category: "Housing"
+  },
+  {
+    id: 3,
+    item_name: "Groceries",
+    amount: 150,
+    date: "2022-01-05",
+    from: "Supermarket",
+    category: "Food"
+  },
+  {
+    id: 4,
+    item_name: "Gas",
+    amount: 35,
+    date: "2022-01-10",
+    from: "Gas Station",
+    category: "Transportation"
+  },
+  {
+    id: 5,
+    item_name: "Bonus",
+    amount: 1000,
+    date: "2022-01-15",
+    from: "Employer",
+    category: "Income"
+  },
+  {
+    id: 6,
+    item_name: "Phone Bill",
+    amount: 80,
+    date: "2022-01-20",
+    from: "Phone Company",
+    category: "Utilities"
+  },
+  {
+    id: 7,
+    item_name: "Dentist",
+    amount: 200,
+    date: "2022-01-25",
+    from: "Dentist",
+    category: "Healthcare"
+  },
+  {
+    id: 8,
+    item_name: "Movie Ticket",
+    amount: 20,
+    date: "2022-01-28",
+    from: "Movie Theater",
+    category: "Entertainment"
+  },
+  {
+    id: 9,
+    item_name: "Savings",
+    amount: 500,
+    date: "2022-01-30",
+    from: "Self",
+    category: "Savings"
+  },
+  {
+    id: 10,
+    item_name: "Gift",
+    amount: 50,
+    date: "2022-01-31",
+    from: "Friend",
+    category: "Gifts"
+  }
+];
